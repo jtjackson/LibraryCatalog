55883ac8d0d11b07bf3a591f379f6248062d0780
Jay Jackson <triplequarks@gmail.com>
2019-04-04 08:21:04 -0500

    Initialize Library Catalog Web App

---
+++ LibraryCatalogWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibraryCatalogWebApp.Startup))]
+namespace LibraryCatalogWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
